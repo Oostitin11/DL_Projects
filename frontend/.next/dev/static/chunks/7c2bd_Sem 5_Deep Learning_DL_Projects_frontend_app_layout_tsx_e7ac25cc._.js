7f3192338028bf1747edfcb01f2c059b69c7f122
kennyjanson1 <kennyjanson1@gmail.com>
2025-12-02 21:18:06 +0700

    Remove .next from git tracking

--- frontend/.next/dev/static/chunks/7c2bd_Sem 5_Deep Learning_DL_Projects_frontend_app_layout_tsx_e7ac25cc._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__1a2e74c3._.css",
-  "static/chunks/79bc2_6ad28bdc._.js"
-],
-    source: "dynamic"
-});
